7e397d0da8b6bf9f4e4857de9a59e9f749a63bbe
Robert Bousquet <rbousquet@newleaders.com>
2013-01-13 01:25:24 -0800

    Refactor flashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def display_flash
+    key = if flash[:error]
+      :error
+    elsif flash[:notice]
+      :notice
+    end
+    content_tag(:div, :class => "flash", :id => "flash#{key.to_s.capitalize}") do
+      content_tag(:h2, flash[key])
+    end
+  end
 end
